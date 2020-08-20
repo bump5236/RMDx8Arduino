3c223c5e5d51eee0d1f0f49671f26e7da2013bd5
bump5236 <t.sanada.330@nitech.jp>
2020-08-20 17:02:37 +0900

    ポジションの読み取り用にbufを用意

--- RMDx8Arduino.cpp
+++ RMDx8Arduino.cpp
@@ -18,13 +18,6 @@ void RMDx8Arduino::canSetup() {
 }
 
 
-void RMDx8Arduino::readBuf(const uint16_t motor_addr) {
-    if (CAN_MSGAVAIL == _CAN.checkReceive()) {
-        _CAN.readMsgBuf(&len, reply_buf);
-    }
-}
-
-
 void RMDx8Arduino::readPID(const uint16_t motor_addr) {
     cmd_buf[0] = 0x30;
     cmd_buf[1] = 0x00;
@@ -38,7 +31,7 @@ void RMDx8Arduino::readPID(const uint16_t motor_addr) {
     // Send message
     writeCmd(motor_addr, cmd_buf);
     delay(100);
-    readBuf(motor_addr);
+    readBuf(motor_addr, cmd_buf);
 }
 
 
@@ -54,7 +47,51 @@ void RMDx8Arduino::writePID(const uint16_t motor_addr, int posKp, int posKi, int
 
     // Send message
     writeCmd(motor_addr, cmd_buf);
-    readBuf(motor_addr);
+    readBuf(motor_addr, cmd_buf);
+}
+
+
+void RMDx8Arduino::readAngle(const uint16_t motor_addr, char n) {
+    switch(n) {
+        case "multi": // multi turns
+            cmd_buf[0] = 0x92;
+            cmd_buf[1] = 0x00;
+            cmd_buf[2] = 0x00;
+            cmd_buf[3] = 0x00;
+            cmd_buf[4] = 0x00;
+            cmd_buf[5] = 0x00;
+            cmd_buf[6] = 0x00;
+            cmd_buf[7] = 0x00;
+            break;
+        
+        default: // single circle
+            cmd_buf[0] = 0x94;
+            cmd_buf[1] = 0x00;
+            cmd_buf[2] = 0x00;
+            cmd_buf[3] = 0x00;
+            cmd_buf[4] = 0x00;
+            cmd_buf[5] = 0x00;
+            cmd_buf[6] = 0x00;
+            cmd_buf[7] = 0x00;
+            break;
+    }
+
+    // Send message
+    writeCmd(motor_addr, cmd_buf);
+    
+    if (CAN_MSGAVAIL == _CAN.checkReceive()) {
+        _CAN.readMsgBuf(&len, unsigned char tmp_buf);
+        if (tmp_buf[0] == cmd_buf[0]) {
+            pos_buf[0] = tmp_buf[0];
+            pos_buf[1] = tmp_buf[1];
+            pos_buf[2] = tmp_buf[2];
+            pos_buf[3] = tmp_buf[3];
+            pos_buf[4] = tmp_buf[4];
+            pos_buf[5] = tmp_buf[5];
+            pos_buf[6] = tmp_buf[6];
+            pos_buf[7] = tmp_buf[7];
+        }
+    }
 }
 
 
@@ -69,7 +106,7 @@ void RMDx8Arduino::clearState(const uint16_t motor_addr) {
     cmd_buf[7] = 0x00;
 
     writeCmd(motor_addr, cmd_buf);
-    readBuf(motor_addr);
+    readBuf(motor_addr, cmd_buf);
 }
 
 
@@ -86,7 +123,7 @@ void RMDx8Arduino::writeCurrent(const uint16_t motor_addr, int16_t current) {
 
     // Send message
     writeCmd(motor_addr, cmd_buf);
-    readBuf(motor_addr);
+    readBuf(motor_addr, cmd_buf);
 }
 
 
@@ -103,7 +140,7 @@ void RMDx8Arduino::writeVelocity(const uint16_t motor_addr, int32_t velocity) {
 
     // Send message
     writeCmd(motor_addr, cmd_buf);
-    readBuf(motor_addr);
+    readBuf(motor_addr, cmd_buf);
 }
 
 
@@ -120,7 +157,7 @@ void RMDx8Arduino::writePosition(const uint16_t motor_addr, int32_t position) {
 
     // Send message
     writeCmd(motor_addr, cmd_buf);
-    readBuf(motor_addr);
+    readBuf(motor_addr, cmd_buf);
 }
 
 
@@ -132,6 +169,23 @@ void RMDx8Arduino::serialWriteTerminator() {
 
 
 // Private
+void RMDx8Arduino::readBuf(const uint16_t motor_addr, unsigned char *buf) {
+    if (CAN_MSGAVAIL == _CAN.checkReceive()) {
+        _CAN.readMsgBuf(&len, unsigned char tmp_buf);
+        if (tmp_buf[0] == buf[0]) {
+            reply_buf[0] = tmp_buf[0];
+            reply_buf[1] = tmp_buf[1];
+            reply_buf[2] = tmp_buf[2];
+            reply_buf[3] = tmp_buf[3];
+            reply_buf[4] = tmp_buf[4];
+            reply_buf[5] = tmp_buf[5];
+            reply_buf[6] = tmp_buf[6];
+            reply_buf[7] = tmp_buf[7];
+        }
+    }
+}
+
+
 void RMDx8Arduino::writeCmd(const uint16_t motor_addr, unsigned char *buf) {
     // CAN通信で送る
     unsigned char sendState = _CAN.sendMsgBuf(motor_addr, 0, 8, buf);
--- RMDx8Arduino.h
+++ RMDx8Arduino.h
@@ -8,16 +8,16 @@
 
 class RMDx8Arduino {
 public:
-    unsigned char len, cmd;
-    unsigned char cmd_buf[8], reply_buf[8];
+    unsigned char len;
+    unsigned char cmd_buf[8], reply_buf[8], pos_buf[8];
 
     RMDx8Arduino(MCP_CAN &CAN);    // クラスと同一の名前にするとコンストラクタ扱い
 
     // Commands
     void canSetup();
-    void readBuf(const uint16_t motor_addr);
     void readPID(const uint16_t motor_addr);
     void writePID(const uint16_t motor_addr, int posKp, int posKi, int velKp, int velKi, int iqKp, int iqKi);
+    void readAngle(const uint16_t motor_addr, char n);
     void clearState(const uint16_t motor_addr);
     void writeCurrent(const uint16_t motor_addr, int16_t current);
     void writeVelocity(const uint16_t motor_addr, int32_t velocity); 
@@ -28,6 +28,7 @@ public:
 
 private:
     MCP_CAN _CAN;
+    void readBuf(const uint16_t motor_addr, unsigned char *buf);
     void writeCmd(const uint16_t motor_addr, unsigned char *buf);
 };
 
